147c42bfd4617dbc36f80136ee12145f7811eeaa
Werner Dweight <wd@wernerdweight.com>
2019-05-21 18:14:34 +0200

    Added tests

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -4,10 +4,82 @@ declare(strict_types=1);
 namespace WernerDweight\Curler\Tests;
 
 use PHPUnit\Framework\TestCase;
+use WernerDweight\Curler\Request;
 
 /**
  * Request tests.
  */
 class RequestTest extends TestCase
 {
+    /**
+     * Tests setEndpoint and getEndpoint.
+     */
+    public function testSetGetEndpoint(): void
+    {
+        $request = new Request();
+        $request->setEndpoint('endpoint');
+        $this->assertSame('endpoint', $request->getEndpoint());
+    }
+
+    /**
+     * Tests setMethod and getMethod.
+     */
+    public function testSetGetMethod(): void
+    {
+        $request = new Request();
+        $request->setMethod('POST');
+        $this->assertSame('POST', $request->getMethod());
+    }
+
+    /**
+     * Tests setPayload and getPayload.
+     */
+    public function testSetGetPayload(): void
+    {
+        $request = new Request();
+        $request->setPayload(['key' => ['value']]);
+        $this->assertSame(['key' => ['value']], $request->getPayload());
+    }
+
+    /**
+     * Tests setHeaders and getHeaders.
+     */
+    public function testSetGetHeaders(): void
+    {
+        $request = new Request();
+        $request->setHeaders(['Content-Type: text/json', 'Origin: localhost']);
+        $this->assertSame(['Content-Type: text/json', 'Origin: localhost'], $request->getHeaders());
+    }
+
+    /**
+     * Tests addHeader.
+     */
+    public function testAddHeader(): void
+    {
+        $request = new Request();
+        $request->setHeaders(['Content-Type: text/json', 'Origin: localhost']);
+        $request->addHeader('X-Requested-By: Test');
+        $this->assertSame(['Content-Type: text/json', 'Origin: localhost', 'X-Requested-By: Test'], $request->getHeaders());
+    }
+
+    /**
+     * Tests removeHeader.
+     */
+    public function testRemoveHeader(): void
+    {
+        $request = new Request();
+        $request->setHeaders(['Content-Type: text/json', 'Origin: localhost', 'X-Requested-By: Test']);
+        $request->removeHeader('X-Requested-By: Test');
+        $this->assertSame(['Content-Type: text/json', 'Origin: localhost'], $request->getHeaders());
+    }
+
+    /**
+     * Tests setAuthentication and getAuthentication.
+     */
+    public function testSetGetAuthentication(): void
+    {
+        $request = new Request();
+        $request->setAuthentication('user', 'password');
+        $this->assertSame(['user' => 'user', 'password' => 'password'], $request->getAuthentication());
+    }
 }
--- Tests/ResponseTest.php
+++ Tests/ResponseTest.php
@@ -4,10 +4,31 @@ declare(strict_types=1);
 namespace WernerDweight\Curler\Tests;
 
 use PHPUnit\Framework\TestCase;
+use WernerDweight\Curler\Response;
 
 /**
  * Response tests.
  */
 class ResponseTest extends TestCase
 {
+    /**
+     * Tests getResponse method.
+     */
+    public function testGetResponse(): void
+    {
+        $response = new Response('any data', []);
+        $this->assertSame('any data', $response->getResponse());
+
+        $response = new Response('{"type":"json","value":[{"id":1},{"id":2}]}', []);
+        $this->assertSame('{"type":"json","value":[{"id":1},{"id":2}]}', $response->getResponse());
+    }
+
+    /**
+     * Tests getMetaData method.
+     */
+    public function testGetMetaData(): void
+    {
+        $response = new Response('', ['http_code' => 200, 'http_status' => 'ok']);
+        $this->assertSame(['http_code' => 200, 'http_status' => 'ok'], $response->getMetaData()->toArray());
+    }
 }
